b09a5b99c9c1da6ad1d14a24ce490376adc6d1cf
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2022-03-29 17:18:59 -0700

    Outline of tictactoe program

---
+++ ttt.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"   
+    "bufio"
+    "os"
+    "strings"
+)
+
+func main() {
+
+    // TODO: V1
+
+    // double 3x3 array
+
+    // random population of array with blank/X/O
+
+    // print random array
+
+    // did anyone win?
+
+    // exit program
+
+
+    // TODO: V2
+
+    // keep running program until a winner occurs, max <input> times
+
+
+    // TODO: V3
+
+    // able to run this program more quickly with threads or processes?
+
+
+    // TODO: v4
+
+    // play against self
+
+
+    // TODO: v5 
+
+    // play against computer that never loses
+
+
+
+    
+
+
+
+    // // Accept input
+    // reader := bufio.NewReader(os.Stdin)
+    // fmt.Print("What is your move? ")
+    // next_move, _ := reader.ReadString('\n')
+
+    // // Only stop accepting input on string "exit"
+    // for next_move != "exit\n" {
+    //     fmt.Print("You moved to " + next_move)
+
+    //     fmt.Println()
+    //     fmt.Print("What is your move? ")
+    //     next_move, _ = reader.ReadString('\n')
+    // }
+
+
+}
+
