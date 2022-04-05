1d73d4836cbfbf0fa700926a136c79391ceb2670
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2022-04-05 13:07:22 -0700

    Dynamic size and wait return with result from goroutine

--- first_go_routine.go
+++ first_go_routine.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 	"runtime"
+	// "sync"
 )
 
 func main() {
@@ -11,19 +12,45 @@ func main() {
 
 	fmt.Println("Started Main")
 
-	var what [1024]int
+	var what [32]chan int
+	// // what[0] = 3
+	// fmt.Printf("%d\n", what[0])
+	// fmt.Printf("%d\n", what[1])
 
 	for itr, _ := range what {
-		go first_go_routine(itr)
+		what[itr] = make(chan int)
+		go calcSquare(itr, what[itr])
 	}
-	time.Sleep(2 * time.Second)
+
+
+	squareOfFour := make(chan int)
+	go calcSquare(4, squareOfFour)
+	fmt.Printf("%T\n", squareOfFour)
+
+	// time.Sleep(2 * time.Second)
 	fmt.Println("Finished Main")
+
+	realSquareOfFour := <- squareOfFour
+	fmt.Printf("%d result\n", realSquareOfFour)
+	fmt.Printf("%T\n", realSquareOfFour)
+	fmt.Printf("%T\n", squareOfFour)
+
+
+	for itr, item := range what {
+		fmt.Printf("%d %d result\n", itr, <-item)
+	}
 }
 
 func first_go_routine(id int) {
 	fmt.Printf("Started Go Routine %d\n", id)
 	time.Sleep(1 * time.Second)
 	fmt.Printf("Finished Go Routine %d\n", id)
+}
+
+func calcSquare(number int, cubeop chan int) {
+	time.Sleep(2 * time.Second)
+	var result = number * number
+	cubeop <- result
 }
 
 // TODO: Test waiting for all goroutines completing
